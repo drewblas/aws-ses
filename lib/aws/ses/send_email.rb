d1f0eb9047766883dca6589c95641fbfb565f6c5
Hans Hasselberg <hans.hasselberg@kaeuferportal.de>
2011-01-31 10:11:12 +0100

    remove whitespaces

--- lib/aws/ses/send_email.rb
+++ lib/aws/ses/send_email.rb
@@ -55,14 +55,6 @@ module AWS
         request('SendEmail', package)
       end
 
-      def send_raw_email(options = {})
-        package = {}
-
-        package['RawMessage.Data'] = Base64::encode64(options[:raw]) if options[:raw]
-
-        request('SendRawEmail', package)
-      end
-
       private
 
       # Adds all elements of the ary with the appropriate member elements

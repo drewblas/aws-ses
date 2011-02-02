7b6591ebfd11250e6a3d686fa2a53d1cbe345f9d
sshaw <sshaw@lucas.cis.temple.edu>
2011-02-02 00:29:12 -0800

    Added request_id to responses, fixed email test case, added address test case, etc...

---
+++ test/address_test.rb
@@ -0,0 +1,73 @@
+require File.dirname(__FILE__) + '/helper'
+
+class AddressTest < Test::Unit::TestCase
+  context 'verifying an address' do
+    setup do
+      @base = generate_base
+    end
+
+    should 'return the correct response on success' do
+      mock_connection(@base, :body => %{
+        <VerifyEmailAddressResponse xmlns="http://ses.amazonaws.com/doc/2010-12-01/">
+          <ResponseMetadata>
+            <RequestId>abc-123</RequestId>
+          </ResponseMetadata>
+        </VerifyEmailAddressResponse>
+      })
+      
+      result = @base.addresses.verify('user1@example.com')
+      assert result.success?
+      assert_equal 'abc-123', result.request_id
+    end  
+  end
+
+  context 'listing verified addressess' do 
+    setup do
+      @base = generate_base
+    end
+    
+    should 'return the correct response on success' do   
+      mock_connection(@base, :body => %{
+        <ListVerifiedEmailAddressesResponse xmlns="http://ses.amazonaws.com/doc/2010-12-01/">
+          <ListVerifiedEmailAddressesResult>
+            <VerifiedEmailAddresses>
+              <member>user1@example.com</member>
+            </VerifiedEmailAddresses>
+          </ListVerifiedEmailAddressesResult>
+          <ResponseMetadata>
+            <RequestId>abc-123</RequestId>
+          </ResponseMetadata>
+        </ListVerifiedEmailAddressesResponse>
+      })
+      
+      result = @base.addresses.list
+
+      assert result.success?
+      assert_equal 'abc-123', result.request_id
+      assert_equal %w{user1@example.com}, result.result
+    end
+  end
+
+
+  context 'deleting a verified addressess' do 
+    setup do
+      @base = generate_base
+    end
+    
+    should 'return the correct response on success' do   
+      mock_connection(@base, :body => %{
+        <DeleteVerifiedEmailAddressResponse xmlns="http://ses.amazonaws.com/doc/2010-12-01/">
+          <ResponseMetadata>
+            <RequestId>abc-123</RequestId>
+          </ResponseMetadata>
+        </DeleteVerifiedEmailAddressResponse>
+      })
+      
+      result = @base.addresses.list
+
+      assert result.success?
+      assert_equal 'abc-123', result.request_id
+    end
+  end
+end
+

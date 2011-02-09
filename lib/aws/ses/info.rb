60c4d5d3f4485abda36f8f4a699312c3884b0290
Drew Blas <drew.blas@isotopetech.com>
2011-02-09 09:25:13 -0600

    Improved info module docs Added Contributors list Moved some docs back into the README.erb

--- lib/aws/ses/info.rb
+++ lib/aws/ses/info.rb
@@ -1,8 +1,54 @@
 module AWS
   module SES
-    # Adds functionality for the statistics and info functionality
+    # Adds functionality for the statistics and info send quota data that Amazon SES makes available
     #
-    # You can call 'quota' or 'statistics'
+    # You can access these methods as follows:
+    #
+    #   ses = AWS::SES::Base.new( ... connection info ... )
+    #
+    # == Get the quota information
+    #   response = ses.quota
+    #   # How many e-mails you've sent in the last 24 hours
+    #   response.send_last_24_hours
+    #   # How many e-mails you're allowed to send in 24 hours
+    #   response.max_24_hour_send
+    #   # How many e-mails you can send per second
+    #   response.max_send_rate
+    #
+    # == Get detailed send statistics 
+    # The result is a list of data points, representing the last two weeks of sending activity.
+    # Each data point in the list contains statistics for a 15-minute interval.
+    # GetSendStatisticsResponse#data_points is an array where each element is a hash with give string keys:
+    #
+    # * +Bounces+
+    # * +DeliveryAttempts+
+    # * +Rejects+
+    # * +Complaints+
+    # * +Timestamp+
+    #
+    #   response = ses.statistics
+    #   response.data_points # =>
+    #       [{"Bounces"=>"0",
+    #         "Timestamp"=>"2011-01-26T16:30:00Z",
+    #         "DeliveryAttempts"=>"1",
+    #         "Rejects"=>"0",
+    #         "Complaints"=>"0"},
+    #        {"Bounces"=>"0",
+    #         "Timestamp"=>"2011-02-09T14:45:00Z",
+    #         "DeliveryAttempts"=>"3",
+    #         "Rejects"=>"0",
+    #         "Complaints"=>"0"},
+    #        {"Bounces"=>"0",
+    #         "Timestamp"=>"2011-01-31T15:30:00Z",
+    #         "DeliveryAttempts"=>"3",
+    #         "Rejects"=>"0",
+    #         "Complaints"=>"0"},
+    #        {"Bounces"=>"0",
+    #         "Timestamp"=>"2011-01-31T16:00:00Z",
+    #         "DeliveryAttempts"=>"3",
+    #         "Rejects"=>"0",
+    #         "Complaints"=>"0"}]
+    
     module Info
       # Returns quota information provided by SES
       # 

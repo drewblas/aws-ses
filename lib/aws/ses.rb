b113bec685aab3cbdb44b60864053b1c05b08fb5
Nathan Bertram <nbertram@gmail.com>
2011-02-09 02:01:07 -0600

    Only add 2.3.x extension if rails 2.3 is defined

--- lib/aws/ses.rb
+++ lib/aws/ses.rb
@@ -22,5 +22,7 @@ require 'ses/base'
 require 'ses/version'
 require 'ses/addresses'
 
-major, minor = Rails.version.split('.')
-require 'actionmailer/ses_extension' if major == '2' && minor == '3'
+if defined?(Rails)
+  major, minor = Rails.version.split('.')
+  require 'actionmailer/ses_extension' if major == '2' && minor == '3'
+end
